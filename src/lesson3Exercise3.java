ca8a580716ac8240423d5b8733d39c611bd4fd57
30112279 <30112279@nescol.ac.uk>
2023-09-13 12:00:34 +0100

    .

---
+++ src/lesson3Exercise3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class lesson3Exercise3 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("what is the cost of the loan? ");
+        String L = input.nextLine();
+
+        System.out.print("what is the interest rate? ");
+        String I = input.nextLine();
+
+        System.out.print("what is the number of years for the loan? ");
+        String N = input.nextLine();
+    }
+}

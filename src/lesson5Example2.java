886d1bde790256fc7409f50fa301b43d3b6de17b
30112279 <30112279@nescol.ac.uk>
2023-09-27 09:51:04 +0100

    .

---
+++ src/lesson5Example2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class lesson5Example2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for( int count=60; count>=10; count-=5 )
+        {
+            System.out.print(count + " ");
+        }
+
+    }
+}

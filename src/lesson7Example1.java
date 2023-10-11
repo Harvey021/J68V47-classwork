576eff9153e9ad3a98ceefbdca6cb059e6e1c474
30112279 <30112279@nescol.ac.uk>
2023-10-11 09:52:46 +0100

    .

---
+++ src/lesson7Example1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class lesson7Example1 {
+    public static void printMessage(){
+        System.out.println("Hello world"); // display “Hello world”
+    }
+    public static void main(String[] args) {
+        printMessage(); // call the printMessage subroutine
+    }
+
+}
